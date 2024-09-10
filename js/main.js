79d3f27b98888ea432a191017d106e51fed4fd27
Ask3Mk <3mask777@gmail.com>
2024-09-10 13:57:05 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+$(".boutton-Youtuop1-boutton").click(function(){
+
+$(".youtube1").hide();
+
+$(this).click(function(){
+    $(".youtube1").show();
+
+});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ---- tist 
+// $(".heelo").click(function(){
+
+//    $("p").hide(); 
+// });
+
+});
